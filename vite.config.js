b666ab87101f87bead94db4cdeda7cdf7f55eab2
EduardoVarela0144 <eduardo.varela142001@gmail.com>
2024-06-11 04:12:03 -0600

    docker file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173
+  },
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
