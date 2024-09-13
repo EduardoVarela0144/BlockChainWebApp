5e28aa9d803b6fc9b81bbff7fceb2848a5d29571
EduardoVarela0144 <eduardo.varela142001@gmail.com>
2024-09-12 19:32:20 -0700

    hide examples

--- src/constants/ItemsMenuDashboard.jsx
+++ src/constants/ItemsMenuDashboard.jsx
@@ -19,8 +19,8 @@ function getItem(label, key, icon) {
 export const ItemsMenuDashboard = [
   getItem(<Link to="/Dashboard">Inicio</Link>, "1", <HomeOutlined />),
   getItem(<Link to="Users">Usuarios</Link>, "2", <UserOutlined />),
-  getItem(<Link to="Example">Ejemplo</Link>, "5", <UserOutlined />),
+  // getItem(<Link to="Example">Ejemplo</Link>, "5", <UserOutlined />),
   getItem(<Link to="Search">Búsqueda</Link>, "3", <SearchOutlined/>),
   getItem(<Link to="/Logout">Cerrar sesión</Link>, "4", <PoweroffOutlined />),
-  getItem(<Link to="Students">Estudiantes</Link>, "6", <PoweroffOutlined />),
+  // getItem(<Link to="Students">Estudiantes</Link>, "6", <PoweroffOutlined />),
 ];
